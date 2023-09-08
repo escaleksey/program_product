7184fa02d5a7be81cee2425bb85af4d395fe578c
fanbumbot <fanbumbot01@mail.ru>
2023-09-08 20:05:15 +0500

    Округление до n-ого знака. Округление изменено до 14-и знаков. Изменена проверка на правильность введённого числа

--- data/calculate.py
+++ data/calculate.py
@@ -1,91 +1,70 @@
 
 from data.complexnumber import *
 
-def check_valid_number(number):
-    if number[0] == '+' or number[0] == '-':
-        number = number[1:].strip()
-        
-    mark = number.find('.')
-    if mark == -1:
-        mark = number.find(',')
-
-    if mark == -1:
-        return number.isdigit()
-    else:
-        return not ((not number[:mark].isdigit()) or (not number[mark+1:].isdigit() and len(number[mark+1:]) != 0))
-
+# Комплексное число из строки
 def parse_from_string(string):
     string = string.strip()
-    
-    mark = string.find("+")
-    if mark <= 0:
-        mark = string.find("-")
-        if mark <= 0:
-            if not check_valid_number(string):
-                return None
-            return ComplexNumber(float(string), 0)
 
-    print(mark)
+    mark = -1
+    char = 0
+    for i in range(len(string)):
+        if string[i] == '+' or string[i] == '-':
+            char = string[i]
+            mark = i
+    if mark == -1:
+        try:
+            return ComplexNumber(float(string), 0)
+        except:
+            return None
 
     number1_string = string[:mark].strip()
-    number2_string = string[mark:].strip()
+    number2_string = char+string[mark+1:].strip()
 
-    print(number1_string + " | " + number2_string[:-1])
-
-    if number2_string[-1] != 'i':
-        return None
-
-    if (not check_valid_number(number1_string)) or (not check_valid_number(number2_string[:-1])):
-        return None
-
-    if number2_string[0] == '+' or number2_string[0] == '-':
-        char = number2_string[0]
-        number2_string = char + number2_string[1:].strip()
-
-    return ComplexNumber(float(number1_string), float(number2_string[:-1]))
-
-def parse_signs_set(number, count_signs):
-    return number
-    '''
-    mark = number.find('.')
+    mark = number1_string.find('i')
     if mark == -1:
-        mark = number.find(',')
+        mark = number2_string.find('i')
         if mark == -1:
-            if count_signs == 0 or count_signs == -1:
-                return number
-            else:
-                return number + '.' + ('0' * count_signs)
-    if count_signs == -1:
-        return number
-    len_string = len(number)-mark-1
-    if len_string <= count_signs:
-        return number + ('0' * count_signs)
-    len_string = len_string-count_signs+1
-    number = number[:-len_string]
-    if not number[-1].isdigit():
-        number = number[:-1]
-    return number
-    '''
+            return None
+    else:
+        if number2_string.find('i') != -1:
+            return None
+        number1_string, number2_string = number2_string, number1_string
 
+    try:
+        return ComplexNumber(float(number1_string), float(number2_string[:-1]))
+    except:
+        return None
+
+# Функция для вывода с учётом количества знаков
+def pre_parse_number(number, count_signs):
+    if count_signs == -1:
+        return str(float(number))
+    if count_signs == 0:
+        count_signs = None
+    return str(round(float(number), count_signs))# Желательно переделать на что-то более стабильное
+
+# Функция, которой на вход даётся список корней, а на выходе получается готовая строка с учётом установленного мода
 def parse_to_string(numbers, mode, count_signs):
     output = str()
     match mode:
         case 0:
-            output = parse_signs_set(str(numbers[0].real), count_signs)
+            output = pre_parse_number(numbers[0].real, count_signs)
         case 1:
             for number in numbers:
-                output += parse_signs_set(str(float(number.real)), count_signs) + " | "
+                output += pre_parse_number(number.real, count_signs) + " | "
             output = output[:-3]
         case 2:
             output = str()
             for number in numbers:
                 if number.imaginary < 0.0:
-                    output += parse_signs_set(str(float(number.real)), count_signs) + " - " + parse_signs_set(str(float(number.imaginary))[1:], count_signs) + "i" + " | "
+                    output += pre_parse_number(round(number.real, 14), count_signs) + " - " + pre_parse_number(round(number.imaginary, 14), count_signs)[1:] + "i" + " | "
                 else:
-                    output += parse_signs_set(str(float(number.real)), count_signs) + " + " + parse_signs_set(str(float(number.imaginary)), count_signs) + "i" + " | "
+                    output += pre_parse_number(round(number.real, 14), count_signs) + " + " + pre_parse_number(round(number.imaginary, 14), count_signs) + "i" + " | "
             output = output[:-3]
+            
     return output
 
+# Выполняется при нажатии на кнопку в GUI
 def result_calculate(UI):
     
     mode = UI.comboBox.currentIndex()
